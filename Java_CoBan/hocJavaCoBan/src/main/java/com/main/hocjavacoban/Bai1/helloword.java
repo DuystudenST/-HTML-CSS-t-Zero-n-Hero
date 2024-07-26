5f314d408c282e1f26d076d269e6f59877273bfb
DuystudentST <duystudent062001@gmail.com>
2024-07-26 20:28:34 +0700

    java cơ bản

---
+++ Java_CoBan/hocJavaCoBan/src/main/java/com/main/hocjavacoban/Bai1/helloword.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.main.hocjavacoban.Bai1;
+
+/**
+ *
+ * @author Admin
+ */
+public class helloword {
+//    Xuất Dữ Liệu Và Nhập Dữ Liệu
+    public static void main(String[] args) {
+        System.out.println("Cộng Hoà Xã Hội Chủ Nghĩa Việt Nam");
+        System.out.println("Độc Lập - Tự Do - Hạnh Phúc");
+    }
+}
---
+++ Java_CoBan/hocJavaCoBan/src/main/java/com/main/hocjavacoban/HocJavaCoBan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.main.hocjavacoban;
+
+/**
+ *
+ * @author Admin
+ */
+public class HocJavaCoBan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
